743bb9c93e2c2a878b761b6cf809dc760de968b8
dewey <mail@notmyhostna.me>
2013-03-20 23:43:46 +0100

    Moved logging level to dev env

--- WhatStatus.js
+++ WhatStatus.js
@@ -21,7 +21,6 @@ app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
-  app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
@@ -31,6 +30,7 @@ app.configure(function(){
 app.configure('development', function(){
   app.use(express.errorHandler());
   app.locals.pretty = true;
+  app.use(express.logger('dev'));
 });
 
 

7b0feed43e1a7b4dc7ce508218341c4f106fbff7
dewey <mail@notmyhostna.me>
2014-11-27 19:42:36 +0100

    Removed Analytics Code

---
+++ public/javascript/piwik-analytics.js
@@ -0,0 +1,12 @@
+var _paq = _paq || [];
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+var u="//analytics.notmyhostna.me/";
+_paq.push(['setTrackerUrl', u+'piwik.php']);
+_paq.push(['setSiteId', 2]);
+var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+})();
+</script>
+<noscript><p><img src="//analytics.notmyhostna.me/piwik.php?idsite=2" style="border:0;" alt="" /></p></noscript>
